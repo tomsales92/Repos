53870a80a09d37c358fc5d385f0b3f1163627f2b
Everton Sales <everton.goncalves92@gmail.com>
2020-07-03 18:03:52 -0300

    Salvando repos

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,4 +1,4 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import {FaGithub, FaPlus, FaSpinner, FaBars, FaTrash} from 'react-icons/fa';
 import {Container, Form, SubmitButton, List, DeleteButton} from './styles';
 
@@ -8,14 +8,37 @@ function Main() {
   const [newRepo, setNewRepo] = useState('');
   const [repositorios, setRepositorios] = useState([]);
   const [loading, setLoading] = useState(false);
+  const [alert, setAlert] = useState(null);
+
+  //Buscar
+useEffect(()=>{
+  const repoStorage = localStorage.getItem('repos');
+  if(repoStorage){
+    setRepositorios(JSON.parse(repoStorage));
+  }
+},[])
+  //Salvar Alterações
+  useEffect(()=>{
+    localStorage.setItem('repos', JSON.stringify(repositorios))
+  }, [repositorios]);
 
     const handleSubmit = useCallback((e)=>{
        e.preventDefault();
 
        async function submit() {
          setLoading(true);
+         setAlert(null);
          try{
+           if(newRepo === ''){
+             throw new Error('Você precisa indicar um repositorio')
+           }
           const response =  await api.get(`repos/${newRepo}`);
+
+          const hasRepo = repositorios.find(repo => repo.name === newRepo);
+
+          if(hasRepo) {
+            throw new Error('Repositorio Duplicado');
+          }
        
           const data = {
              name: response.data.full_name,
@@ -24,6 +47,7 @@ function Main() {
          setRepositorios([...repositorios, data]);
          setNewRepo('');
          }catch(error) {
+           setAlert(true);
           console.log(error)
          }finally{
             setLoading(false);
@@ -35,6 +59,7 @@ function Main() {
 
   async function handleInputChange(e){
     setNewRepo(e.target.value);
+    setAlert(null);
   }
 
   const handleDelete = useCallback((repo)=>{
@@ -49,7 +74,7 @@ function Main() {
          <FaGithub size={25} />
          Meus Repositórios
         </h1>
-        <Form onSubmit={handleSubmit}>
+        <Form onSubmit={handleSubmit} error={alert}>
           <input
            type="text"
            placeholder="Adicionar Repositorio"
